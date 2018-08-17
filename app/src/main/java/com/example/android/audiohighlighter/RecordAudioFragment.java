3c561b2dc731d1880520510ad457df180ed483d6
Thomas Santopadre <tsanto2.ts@gmail.com>
2018-08-16 21:18:47 -0500

    Test in-app purchase implemented.

--- app/src/main/java/com/example/android/audiohighlighter/RecordAudioFragment.java
+++ app/src/main/java/com/example/android/audiohighlighter/RecordAudioFragment.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,15 +14,21 @@ import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,7 +42,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class RecordAudioFragment extends Fragment {
+public class RecordAudioFragment extends Fragment implements BillingProcessor.IBillingHandler{
 
     private View view;
     private boolean isRecording, isPaused;
@@ -52,6 +59,8 @@ public class RecordAudioFragment extends Fragment {
     private View recordAudioButton;
     private long startTime, currTime, pauseStartTime, pauseTimeMillis;
     private String newTitle;
+
+    private BillingProcessor bp;
 
     private int sampleRate;
 
@@ -80,6 +89,9 @@ public class RecordAudioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view = inflater.inflate(R.layout.fragment_record_audio, container, false);
+
+        bp = new BillingProcessor(getContext(), null, this);
+
         internalDirectory = getContext().getFilesDir();
         timestamps = new ArrayList<>();
 
@@ -94,6 +106,7 @@ public class RecordAudioFragment extends Fragment {
         SetRecordTimestampButtonListener();
         SetSaveRecordingButtonListener();
         SetTitleTextTouchedListener();
+        SetPurchaseButtonListener();
 
         return view;
     }
@@ -382,4 +395,49 @@ public class RecordAudioFragment extends Fragment {
         });
     }
 
+    void SetPurchaseButtonListener(){
+        Button btn = (Button)view.findViewById(R.id.purchase_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bp.purchase(getActivity(), "premium_test.1");
+            }
+        });
+    }
+
+    @Override
+    public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
+        Button btn = view.findViewById(R.id.purchase_btn);
+        btn.setText("SUCCESS!");
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, @Nullable Throwable error) {
+
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (!bp.handleActivityResult(requestCode, resultCode, data)){
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        if (bp != null){
+            bp.release();
+        }
+        super.onDestroy();
+    }
 }
